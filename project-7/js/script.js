21b7ae19f870493cfeebc9c7008f26affdad1195
erikova <88664731+erikova@users.noreply.github.com>
2021-08-19 18:12:34 +0600

    Add files via upload

---
+++ project-7/js/script.js
@@ -0,0 +1,14 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        }
+    }
+});
